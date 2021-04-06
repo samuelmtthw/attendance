4124f6509492b3cee9704227ecc1ab3f0eead985
Samuel Matthew <samuelmatthew211@gmail.com>
2021-04-06 16:21:37 +0700

    :package: Add 3 new faces (Michelle, Jokowi, Jacob) to the dataset and update the mode

---
+++ test.py
@@ -0,0 +1,24 @@
+# timesDetected = {"0321001" : 320,
+#                  "0321002" : 0,
+#                  "0321003" : 20,
+#                  "0321004" : 0,
+#                  "0321005" : 30,
+#                  "0321006" : 0,
+#                  "0321007" : 0,
+#                  "0321008" : 0
+# }
+
+
+
+# for time in timesDetected:
+#     if timesDetected[time] >= 300:
+#         print("YES")
+#         print("face id is: " + time)
+#     else:
+        
+todayStart = [1,2,3,4,5]
+
+name = 6
+
+if name not in todayStart:
+    print("NAME IS NOT IN THE LIST")
